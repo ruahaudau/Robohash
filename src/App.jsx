b68831a33d81387abb0e419b23689c5c03612d47
ruahaudau <tranhungvt@gmail.com>
2025-04-29 11:47:12 +0700

    Adding SearchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import CardList from './component/card-list/card-list.component'
-
+import './App.scss';
+import CardList from './component/card-list/card-list.component';
+import SearchBox from './component/search-box/search-box.component';
 
 function App() {
 
   const [monsters, setMonster] = useState([]);
+  const [searchMonster, setSearchMonster] = useState([]);
 
   useEffect(() => {
     const response = fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((data) =>setMonster(data))
+      .then((data) =>{
+        setMonster(data)
+        setSearchMonster(data)})
     }, []);
-  console.log(monsters);
+
+  const onChangeHandler = (event)=>{
+    const filterString= event.target.value.toLowerCase();
+    const newFilterMonster = monsters.filter((monster)=>
+      monster.name.toLowerCase().includes(filterString)
+    );
+    setSearchMonster(newFilterMonster);
+  }
+
 
   return (
-    <CardList monsters={monsters}></CardList>
+    <>
+      <SearchBox onChangeHandler={onChangeHandler}/>
+      <CardList monsters={searchMonster}></CardList>
+    </>
   )
 }
 
